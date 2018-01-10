c32738673408c53cf405d94d2b5628758f2163b8
vetoni <thajdari@cask.co>
2018-01-10 09:30:54 -0600

    initial commit

--- Pipeline.py
+++ Pipeline.py
@@ -8,7 +8,7 @@ from datetime import datetime
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
-                    filename='mario.log',
+                    filename=__name__ + '.log',
                     filemode='w')
 
 # write INFO messages or higher to the sys.stderr
